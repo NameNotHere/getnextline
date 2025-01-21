a8b38f9ab06b99132aa5eabaa5d1c233ea2c0331
Orhan Tanovic <otanovic@f0r3s2.codam.nl>
2025-01-21 19:24:51 +0100

    fixed 1000

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: otanovic <otanovic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 15:52:02 by otanovic          #+#    #+#             */
-/*   Updated: 2025/01/21 16:23:36 by otanovic         ###   ########.fr       */
+/*   Updated: 2025/01/21 19:24:39 by otanovic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,26 @@
 # define BUFFER_SIZE 50
 #endif
 
-void	get_the_line(char	**remainder, ssize_t *bytes_read, int fd)
+void	*get_the_line(char	**remainder, ssize_t *bytes_read, int fd)
 {
-	char	*temp;
-	char	buffer[BUFFER_SIZE + 1];
+	char		*temp;
+	static char	buffer[BUFFER_SIZE + 1];
 
 	*bytes_read = read(fd, buffer, BUFFER_SIZE);
 	if (*bytes_read > 0)
 	{
 		buffer[*bytes_read] = '\0';
-		temp = *remainder;
 		if (!*remainder)
 			*remainder = ft_strdup(buffer);
 		else
+		{
+			temp = *remainder;
 			*remainder = ft_strjoin(*remainder, buffer, 0, 0);
+		}
 		free_and_return_null(&temp);
+		return (remainder);
 	}
+	return (NULL);
 }
 
 char	*process_remainder(int fd, ssize_t *bytes_read, char **remainder, \
@@ -43,16 +47,17 @@ char	*process_remainder(int fd, ssize_t *bytes_read, char **remainder, \
 	char	*line;
 	char	*temp;
 
-	while (*bytes_read > 0 || (*remainder && (*remainder)[0] != '\0'))
+	while (*bytes_read > 0 || (remainder && (*remainder)[0] != '\0'))
 	{
 		get_the_line(remainder, bytes_read, fd);
-		if (!*remainder)
-			return (NULL);
 		newline_pos = ft_strchr(*remainder, '\n');
 		if (newline_pos)
 		{
 			*newline_pos = '\0';
-			line = ft_strjoin(*remainder, "\n", 0, 0);			
+			if (!*remainder)
+				return (NULL);
+			else
+				line = ft_strjoin(*remainder, "\n", 0, 0);
 			temp = *remainder;
 			*remainder = ft_strdup(newline_pos + 1);
 			free_and_return_null(&temp);
@@ -73,6 +78,10 @@ char	*process_line(int fd, ssize_t *bytes_read)
 	static char		*remainder;
 	char			*line;
 
+	if (fd <= 0 || BUFFER_SIZE <= 0)
+		return (free_and_return_null(&remainder));
+	if (!remainder)
+		remainder = ft_strdup("");
 	line = process_remainder(fd, bytes_read, &remainder, NULL);
 	if (line == NULL && remainder && remainder[0] != '\0')
 	{
