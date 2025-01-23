d49423760b869d8d4c0716f6115028995c4d0097
Orhan Tanovic <otanovic@f0r3s4.codam.nl>
2025-01-23 16:14:52 +0100

    passes all tests

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,28 @@
 /*   By: otanovic <otanovic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 15:52:02 by otanovic          #+#    #+#             */
-/*   Updated: 2025/01/22 20:04:24 by otanovic         ###   ########.fr       */
+/*   Updated: 2025/01/23 16:02:32 by otanovic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
 #include "get_next_line.h"
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 50
+# define BUFFER_SIZE 42
 #endif
 
 void	get_the_line(char	**remainder, ssize_t *bytes_read, int fd)
 {
 	char		*temp;
-	static char	buffer[BUFFER_SIZE];
+	char		*buffer;
 
+	buffer = malloc(BUFFER_SIZE * sizeof(char) + 1);
+	if (!buffer)
+		return ;
 	*bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (*bytes_read >= 0)
+	if (*bytes_read > 0)
 	{
 		buffer[*bytes_read] = '\0';
 		if (!*remainder)
@@ -36,9 +38,10 @@ void	get_the_line(char	**remainder, ssize_t *bytes_read, int fd)
 			*remainder = ft_strjoin(*remainder, buffer, 0, 0);
 			free_and_return_null(&temp);
 		}
-		//return (remainder);
+		free_and_return_null(&buffer);
+		return ;
 	}
-//	return (NULL);
+	free_and_return_null(&buffer);
 }
 
 char	*process_remainder(int fd, ssize_t *bytes_read, char **remainder, \
@@ -47,21 +50,17 @@ char	*process_remainder(int fd, ssize_t *bytes_read, char **remainder, \
 	char	*line;
 	char	*temp;
 
-	while (*bytes_read > 0 || (remainder && (*remainder)[0] != '\0'))
+	get_the_line(remainder, bytes_read, fd);
+	while (*bytes_read >= 0 && (*remainder && (*remainder)[0] != '\0'))
 	{
-		get_the_line(remainder, bytes_read, fd);
-
 		newline_pos = ft_strchr(*remainder, '\n');
-		if (newline_pos)
+		if (newline_pos && *remainder)
 		{
 			*newline_pos = '\0';
-			if (!*remainder)
-				return (NULL);
-			else
-				line = ft_strjoin(*remainder, "\n", 0, 0);
+			line = ft_strjoin(*remainder, "\n", 0, 0);
 			temp = *remainder;
 			*remainder = ft_strdup(newline_pos + 1);
-			free_and_return_null(&temp);
+			free(temp);
 			return (line);
 		}
 		else if (*remainder && (*remainder)[0] != '\0' && *bytes_read == 0)
@@ -70,22 +69,22 @@ char	*process_remainder(int fd, ssize_t *bytes_read, char **remainder, \
 			free_and_return_null(remainder);
 			return (line);
 		}
+		get_the_line(remainder, bytes_read, fd);
 	}
-	return (NULL);
+	return (free_and_return_null(remainder));
 }
 
-char	*process_line(int fd, ssize_t *bytes_read)
+char	*process_line(int fd, ssize_t *bytes_read, char	**remainder)
 {
-	static char		*remainder;
 	char			*line;
 
-	if (!remainder)
-		remainder = ft_strdup("");
-	line = process_remainder(fd, bytes_read, &remainder, NULL);
-	if (line == NULL && remainder && remainder[0] != '\0')
+	if (!*remainder)
+		*remainder = ft_strdup("");
+	line = process_remainder(fd, bytes_read, remainder, NULL);
+	if (line == NULL && *remainder && *remainder[0] != '\0')
 	{
-		line = ft_strdup(remainder);
-		free_and_return_null(&remainder);
+		line = ft_strdup(*remainder);
+		free(remainder);
 	}
 	if (line == NULL)
 		return (NULL);
@@ -96,12 +95,13 @@ char	*get_next_line(int fd)
 {
 	ssize_t			bytes_read;
 	char			*line;
+	static char		*remainder;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 9223372)
-		return (NULL);
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > _SC_SSIZE_MAX)
+		return (free_and_return_null(&remainder));
 	bytes_read = 1;
-	line = process_line(fd, &bytes_read);
+	line = process_line(fd, &bytes_read, &remainder);
 	if (!line)
-		return (NULL);
+		return (free_and_return_null(&remainder));
 	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: otanovic <otanovic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 15:52:16 by otanovic          #+#    #+#             */
-/*   Updated: 2025/01/20 14:12:17 by otanovic         ###   ########.fr       */
+/*   Updated: 2025/01/23 14:27:27 by otanovic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		ft_len(char *s);
 char	*ft_strjoin(char const *s1, char const *s2, int i, int j);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *str);
-char	*process_line(int fd, ssize_t *bytes_read);
+char	*process_line(int fd, ssize_t *bytes_read, char	**remainder);
 char	*get_next_line(int fd);
 
 #endif
